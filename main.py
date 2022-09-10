b92a744590053ff8e02ebbeba8d0e3d39620fb32
AAverUrfu <113232940+AAverUrfu@users.noreply.github.com>
2022-09-10 08:15:24 +0000

    пункт 1. Создание класса Фигура

--- main.py
+++ main.py
@@ -3,5 +3,8 @@ class Figure():
     print("Создание figure.")
   def show(self):
     print("I am figure!")
+  def perimeter(self):
+    print("вычисление периметра.")
 f = Figure()
 f.show()
+f.perimeter()
